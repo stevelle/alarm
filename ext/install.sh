d169a9c6b931aa4fc0b7af3d32c7e468c2bb5b6b
Steve Lewis <stevelle@gmail.com>
2015-03-19 18:23:20 -0500

    adding requirements.txt, a supervisor script, and an install script for the service

---
+++ ext/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+# install dependencies
+apt-get update
+apt-get -y install build-essential python python-dev python-virtualenv 
+supervisor git
+
+# create a user to run the agent 
+adduser --disabled-password --gecos "" loadmonitor
+cd /home/loadmonitor
+
+gent source
+git clone https://github.com/stevelle/loadmonitor.git 
+cp loadmonitor/monitor.py .
+
+# create a virtualenv and install dependencies
+virtualenv venv
+venv/bin/pip install -r loadmonitor/requirements.txt
+
+chown -R loadmonitor:loadmonitor . 
+
+# configure supervisor to run the service
+cp loadmonitor/ext/supervisor/loadmonitor.conf /etc/supervisor/conf.d/
+mkdir -p /var/log/loadmonitor
+supervisorctl reread
+supervisorctl update
+
